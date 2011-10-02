5b20fc5f6d63372271ce72984b862e47ee0f20e6
Alexandre Bertails <bertails@gmail.com>
2011-10-02 19:40:47 -0400

    ~ update to sbt 0.11 and scala 2.9.1. weird error when dispatch only in test scope

--- project/build.scala
+++ project/build.scala
@@ -4,17 +4,16 @@ import Keys._
 // some usefull libraries
 // they are pulled only if used
 object Dependencies {
-  val specs = "org.scala-tools.testing" % "specs_2.9.0-1" % "1.6.8" % "test"
-  val scalatest = "org.scalatest" % "scalatest_2.9.0" % "1.6.1" % "test"
-  val dispatch = "net.databinder" %% "dispatch-http" % "0.8.4" % "test"
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.9" % "test"
+  val dispatch = "net.databinder" %% "dispatch-http" % "0.8.5" % "test"
   val unfiltered_filter = "net.databinder" %% "unfiltered-filter" % "0.4.1"
   val unfiltered_jetty = "net.databinder" %% "unfiltered-jetty" % "0.4.1"
   val unfiltered_spec = "net.databinder" %% "unfiltered-spec" % "0.4.1" % "test"
   val slf4jSimple = "org.slf4j" % "slf4j-simple" % "1.5.8"
-  val antiXML = "com.codecommit" %% "anti-xml" % "0.3-SNAPSHOT" % "test"
+  val antiXML = "com.codecommit" %% "anti-xml" % "0.4-SNAPSHOT" % "test"
   val jena = "com.hp.hpl.jena" % "jena" % "2.6.4"
   val arq = "com.hp.hpl.jena" % "arq" % "2.8.8"
-  val grizzled = "org.clapper" %% "grizzled-scala" % "1.0.7" % "test"
+  val grizzled = "org.clapper" %% "grizzled-scala" % "1.0.8" % "test"
   val scalaz = "org.scalaz" %% "scalaz-core" % "6.0.2"
 }
 
@@ -28,7 +27,7 @@ object BuildSettings {
 
   val buildOrganization = "org.w3"
   val buildVersion      = "0.1-SNAPSHOT"
-  val buildScalaVersion = "2.9.0-1"
+  val buildScalaVersion = "2.9.1"
 
   val buildSettings = Defaults.defaultSettings ++ Seq (
     organization := buildOrganization,
@@ -47,6 +46,8 @@ object YourProjectBuild extends Build {
   import BuildSettings._
   import ProguardPlugin._
   import sbtassembly.Plugin._
+  import sbtassembly.Plugin.AssemblyKeys._
+  
 
   def keepUnder(pakage:String):String = "-keep class %s.**" format pakage
   
@@ -76,7 +77,7 @@ object YourProjectBuild extends Build {
       libraryDependencies += antiXML,
       libraryDependencies += grizzled,
       libraryDependencies += scalaz,
-      jarName in Assembly := "read-write-web.jar"
+      jarName in assembly := "read-write-web.jar"
     )
 
   lazy val project = Project(
--- project/plugins/build.sbt
+++ project/plugins/build.sbt
@@ -1,16 +1,10 @@
-libraryDependencies <+= (sbtVersion) { sv => "com.eed3si9n" %% "sbt-assembly" % ("sbt" + sv + "_0.4") }
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.7.0")
 
-resolvers += {
-  val typesafeRepoUrl = new java.net.URL("http://repo.typesafe.com/typesafe/releases")
-  val pattern = Patterns(false, "[organisation]/[module]/[sbtversion]/[revision]/[type]s/[module](-[classifier])-[revision].[ext]")
-  Resolver.url("Typesafe Repository", typesafeRepoUrl)(pattern)
-}
+resolvers += Classpaths.typesafeResolver
 
-libraryDependencies <<= (libraryDependencies, sbtVersion) { (deps, version) => 
-  deps :+ ("com.typesafe.sbteclipse" %% "sbteclipse" % "1.3-RC3" extra("sbtversion" -> version))
-}
+addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse" % "1.4.0")
 
 resolvers += "Proguard plugin repo" at "http://siasia.github.com/maven2"
 
-libraryDependencies <+= sbtVersion("com.github.siasia" %% "xsbt-proguard-plugin" % _)
+addSbtPlugin("com.github.siasia" % "xsbt-proguard-plugin" % "0.1")
 
