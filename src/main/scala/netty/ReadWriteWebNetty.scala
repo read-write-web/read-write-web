b4ac7b095f23c87d90e418dbd0b35a3fcff43f2c
Henry Story <henry.story@bblfish.net>
2012-01-13 16:04:39 +0100

    somehow Intellij replaced the original copyright statements with mine. probably when moving directory

--- src/main/scala/netty/ReadWriteWebNetty.scala
+++ src/main/scala/netty/ReadWriteWebNetty.scala
@@ -35,7 +35,6 @@ import java.io.{InputStream, OutputStream}
 import unfiltered.response._
 import unfiltered.netty._
 import collection.immutable.List
-import util.{NetworkLoggingSM, AllowAllSecurityManager}
 
 /**
  * ReadWrite Web for Netty server, allowing TLS renegotiation
